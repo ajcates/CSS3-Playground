dc4adcca0622f5cefd58aa0cee012792becd38ca
A.J. Cates <aj@ajcates.com>
2010-08-04 00:57:20 -0700

    got it lookin some what pretty

--- index.php
+++ index.php
@@ -10,55 +10,37 @@
 		<h2>&amp; some php5.3</h2>
 	</header>
 	<article>
-		
+		<aside>
+			<h2>What?</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</aside>
 		<h2>CSS3 Wrapping Drop Shadows</h2>
-		<a href="#" class="button">Button</a>
-		<a href="#" class="button">Button with slightly longer text.</a>
-		<pre>
-<?
-		
-		
-		function f_map($func, $val) {
-			return array_map($func, $val);
-		}
-		echo join(f_map(
-			function($v) {
-				return $v . 'lol, ';
-			},
-			range(0, 150)
-		));
-		
-		echo "\n\n";
-	
-		$test = array();
-		$test2 = array(null);
-
-		if(empty($test)) {
-			echo 'test is empty';
-		}
-		echo "\n\n";
-		if(empty($test2)) {
-			echo 'test2 is empty';
-		}
-
-		
+		<a href="#" class="button">@Bennie's</a>
+		<a href="#" class="button">Buttons</a>
+		<pre><?
 			
-		$array1 = array(0 => 'zero_a', 2 => 'two_a', 3 => 'three_a');
-		$array2 = array(1 => 'one_b', 3 => 'three_b', 4 => 'four_b');
-		
-		$result = $array1 + $array2;
-		var_dump($result);
-		
-		$result2 = array_merge($array1, $array2);
-		var_dump($result2);
-		
-		?>
-		</pre>
+		//	print_r(range('a', 'z'));
+			
+			$alphabet = range('a', 'z');
+			$backwardsAlphabet = array_reverse($alphabet);
+			print_r(array_map(
+				function($a) use($backwardsAlphabet) {
+					static $count = 0;
+					return join(range($a, $backwardsAlphabet[$count++]));
+				},
+				$alphabet
+			));
+			
+
+		?></pre>
 		<label for="testbox">Checkbox1</label>
 		<input type="checkbox" name="checkboxtest" value="test" id="testbox" />
 		<label for="testbox2">Checkbox2</label>
 		<input type="checkbox" name="checkboxtest" value="other" id="testbox2" />
 		
 	</article>
+	<footer>
+		<p>Created by ajcates - New BSD License</p>
+	</footer>
 </body>
 </html>
