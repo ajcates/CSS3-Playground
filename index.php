59eb5e194f4e5d2577319c446527bba380645f87
A.J. Cates <aj@ajcates.com>
2010-08-10 09:33:06 -0700

    added in ryans ul centering menu

--- index.php
+++ index.php
@@ -20,7 +20,14 @@
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 		<a href="#" class="button purple">@Bennie's</a>
 		<a href="#" class="button purple">Buttons</a>
-		
+		<ul id="centerList">
+			<li>Item 1</li>
+			<li>Item 2</li>
+			<li>Item 3</li>
+			<li>Item 4</li>
+			<li>Item 5</li>
+			<li>Item 6</li>
+		</ul>	
 		<h3>Things I want add:</h3>
 		<ul>
 			<li>Fancy lists styles</li>
